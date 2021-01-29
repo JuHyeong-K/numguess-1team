299f48d0dd70279c9c7779af6686f34c8b061ebc
ebchoi <eunbichoi@outlook.com>
2021-01-29 17:22:09 +0900

    feat: Add if statement
    Add if statement for numguess game to compare answer and user input
    (guess)

--- numguess.py
+++ numguess.py
@@ -2,3 +2,11 @@ import random
 
 answer = random.randint(1,100)
 print(answer, type(answer))
+
+guess = 20
+#TO DELETE UPON MERGE
+
+if answer-guess==0:
+    print("CORRECT!")
+else:
+    print("WRONG! The correct number is {}.".format(answer))
